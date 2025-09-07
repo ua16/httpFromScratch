6ef650ede798eaa7cd1b8d6c600b47de5a61c1c4
ua16 <ua16@users.noreply.github.com>
2025-09-07 13:59:06 +0530

    Added MIME type defaults with C99 style list thingies

--- funcs.h
+++ funcs.h
@@ -1,10 +1,28 @@
 #ifndef FUNC_H
 #define FUNC_H
 
+typedef enum { TEXT, IMAGE } ContentType;
 
-typedef ContentType { TEXT, IMAGE,  }
+#define NUMFILETYPES 10
+typedef enum {
+    FILE_DEFAULT,
+    FILE_HTML,
+    FILE_JS,
+    FILE_CSS,
+    FILE_PNG,
+    FILE_BMP,
+    FILE_GIF,
+    FILE_JPG
+} FileType;
 
-char * getFileExtension(char * strStart, int maxLen); // Get the file extension of a string
-char * getFileType(char * strStart, int maxLen); // A wrapper for the above
+const char *fileTypes[NUMFILETYPES] = {
+    [FILE_DEFAULT] = "plain", [FILE_HTML] = "html", [FILE_JS] = "javascript",
+    [FILE_CSS] = "css",       [FILE_PNG] = "png",   [FILE_BMP] = "bmp",
+    [FILE_GIF] = "gif",       [FILE_JPG] = "jpg",
+};
+
+char *getFileExtension(char *strStart,
+                       int maxLen); // Get the file extension of a string
+FileType *getFileType(char *strStart, int maxLen); // A wrapper for the above
 
 #endif // !FUNC_H
